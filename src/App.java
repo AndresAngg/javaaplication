47ac919691c7df9a9c685077a3c8ae2e785e325e
Andrés <andresangg@gmail.com>
2021-09-13 23:27:12 -0500

    Last version

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
     //  ejercicio1 E = new ejercicio1();
-      ejercicio9 e3 = new ejercicio9();
+      ejercicio10 e3 = new ejercicio10();
     //  E.main();
       e3.main();
     }
---
+++ src/ejercicio10.java
@@ -0,0 +1,15 @@
+public class ejercicio10 {
+
+    public static void main(){
+        String va1="Pedro",va2="Joly",va3;
+
+        va3 = va1 + va2;
+
+        System.out.println(va3);
+        /* Se estan concatenando las dos cadenas de la primera con la segunda */
+        va3 = va1 va2;
+        System.out.println(va3);
+        /* Aqui se presenta error de sintaxis*/
+    }
+    
+}
